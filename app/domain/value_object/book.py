660f03776046bea626582b34b2d2d41337e35289
Squilix01 <Squilix01@no-reply.com>
2025-08-28 23:32:20 +0300

    introduce value objects for book and chapter with basic validation logic

---
+++ app/domain/value_object/book.py
@@ -0,0 +1,25 @@
+from app.domain.enum.difficulty import Difficulty
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class BookVO:
+
+    difficulty: Difficulty | None
+
+    def __post_init__(self):
+        if self.difficulty is not None:
+            if not isinstance(self.difficulty, Difficulty):
+                try:
+                    self.difficulty = Difficulty(self.difficulty)
+                except Exception:
+                    raise ValueError(f"Неверное значение difficulty: {self.difficulty}")
+        else:
+            self.difficulty = None
+        if self.title.strip() == "":
+            raise ValueError("Заголовок(title) не может содержать только пробелы")
+        if self.description.strip() == "":
+            raise ValueError("Описание(description) не может содержать только пробелы")
+        
+
+    def __str__(self):
+        return self.difficulty
---
+++ app/domain/value_object/chapter.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ChapterVO():
+    title: str | None
+    content: str | None
+    
+
+    def __post_init__(self):
+        if self.content is None:
+            raise ValueError("Содержимое(content) не может быть пустой")
+        if self.title is None:
+            raise ValueError("Заголовок(title) не может быть пустой")
+        if self.title.strip() == "":
+            raise ValueError("Заголовок(title) не может содержать только пробелы")
+        if self.content.strip() == "":
+            raise ValueError("Содержимое(content) не может содержать только пробелы")
+        
+    def __str__(self):
+        return f"{self.title}: {self.content[:30]}..."
