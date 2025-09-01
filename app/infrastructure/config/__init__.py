164ee861da5eaab955cbb3aa4bf81416bd900e39
Squilix01 <Squilix01@no-reply.com>
2025-09-01 21:13:10 +0300

    feat(infra) add config for project use pydantic

---
+++ app/infrastructure/config/__init__.py
@@ -0,0 +1,8 @@
+from .config import Config, DatabaseConfig, AppConfig
+
+__all__ = [
+    "Config",
+    "DatabaseConfig",
+    "AppConfig",
+    "get_config",
+]
---
+++ app/infrastructure/config/config.py
@@ -0,0 +1,41 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy import URL
+
+
+class DatabaseConfig(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
+
+    def get_database_url(self) -> URL:
+        return URL.create(
+            drivername="postgresql+asyncpg",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            database=self.DB_NAME,
+        )
+
+    model_config = {
+        "extra": "ignore",
+        "env_file_encoding": "utf-8",
+    }
+
+
+class AppConfig(BaseSettings):
+    DEBUG: bool = True
+    ENVIRONMENT: str = "production" 
+    SECRET_KEY: str = "dev-secret" 
+
+    model_config = {
+        "extra": "ignore",
+        "env_file_encoding": "utf-8",
+    }
+
+
+class Config:
+
+    def __init__(self, env_file: str | None = None):
+        self.app = AppConfig(_env_file=env_file)
+        self.db = DatabaseConfig(_env_file=env_file)
