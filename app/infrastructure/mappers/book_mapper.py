113981ee11bb1709324879888cea3accecf0ca57
Squilix01 <Squilix01@no-reply.com>
2025-09-05 23:08:00 +0300

    feat(infra): Implement BookRepository with mappers

---
+++ app/infrastructure/mappers/book_mapper.py
@@ -0,0 +1,24 @@
+from app.domain.entity.book import Book as BookDomain
+from app.infrastructure.models.book_model import Book as BookModel
+
+
+def orm_to_domain(orm: BookModel) -> BookDomain:
+    return BookModel(
+        id=orm.id,
+        title=orm.title,
+        author=orm.author,
+        description=orm.description,
+        pic_url=orm.pic_url,
+        difficulty=orm.difficulty
+    )
+
+
+def domain_to_orm(domain: BookDomain):
+    return BookModel(
+        id=domain.id,
+        title=domain.title,
+        author=domain.author,
+        description=domain.description,
+        pic_url=domain.pic_url,
+        difficulty=domain.difficulty
+    )
---
+++ app/infrastructure/repositories/book_repositories.py
@@ -0,0 +1,66 @@
+from sqlalchemy import select, delete, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
+
+from app.domain.protocols.book_protocol import AbstractBookProtocol
+from app.domain.entity.book import Book as BookDomain
+from app.infrastructure.models.book_model import Book as BookModel
+from app.infrastructure.mappers.book_mapper import orm_to_domain, domain_to_orm
+
+
+class BookRepository(AbstractBookProtocol):
+    def __init__(self, session_factory: AsyncSession):
+        self.session_factory = session_factory
+
+    async def add(self, book: BookDomain):
+        orm = domain_to_orm(book)
+        self.session_factory.add(orm)
+        await self.session_factory.commit()
+        await self.session_factory.refresh(orm)
+        return orm_to_domain(orm)
+    
+    async def get(self, book_id: int):
+        stmt = select(BookModel).where(BookModel.id == book_id)
+        result = await self.session_factory.execute(stmt)
+        orm = result.scalars().one_or_none()
+        if not orm:
+            return None
+        return orm_to_domain(orm)
+    
+    async def list(self, limit: int = 20, cursor: Optional[str] = None):
+        stmt = select(BookModel).order_by(BookModel.id.asc()).limit(limit)
+        if cursor:
+            stmt = stmt.where(BookModel.id > cursor)
+        result = await self.session_factory.execute(stmt)
+        rows = result.scalars().all()
+        return [orm_to_domain(row)
+            for row in rows
+        ]
+    
+    async def update(self, book_id: int, title: Optional[str], author: Optional[str], description: Optional[str], pic_url: Optional[str],  difficulty: Optional[str]):
+        stmt = update(BookModel).where(BookModel.id == book_id).values(
+            title=title, author=author, description=description, pic_url=pic_url, difficulty=difficulty
+        )
+        await self.session_factory.execute(stmt)
+        await self.session_factory.commit()
+
+        stmt_select = select(BookModel).where(BookModel.id == book_id)
+        result = await self.session_factory.execute(stmt_select)
+        orm = result.scalars().one()
+        return orm_to_domain(orm)
+
+    async def delete(self, book_id: int):
+        stmt = delete(BookModel).where(BookModel.id == book_id)
+        await self.session_factory.execute(stmt)
+        await self.session_factory.commit()
+
+    async def find_by_title_author(self, title, author):
+        stmt = select(BookModel).where(
+            BookModel.title == title,
+            BookModel.author == author
+            )
+        result = await self.session_factory.execute(stmt)
+        orm = result.scalar_one_or_none()
+        if not orm:
+            return None
+        return orm_to_domain(orm)
